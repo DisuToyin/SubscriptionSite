f771a8cc990b681162dece4bcf1d172346ac0950
DisuToyin <disujt@gmail.com>
2022-08-10 02:29:38 +0100

    added homepage plus new routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Register } from "./pages/Register";
 import { Signin } from "./pages/Signin";
+import Home from "./pages/Home";
+import PasswordReset from "./pages/PasswordReset";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signin />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/password-reset" element={<PasswordReset />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <a href="/signin">Sign in page</a>
+      <a href="/register">Register page</a>
+      <a href="/password-reset">Password reset</a>
+    </div>
+  );
+}
