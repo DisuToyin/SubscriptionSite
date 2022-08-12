9806d604dbc4db53b39dd79e9ae4812b19a79137
DisuToyin <disujt@gmail.com>
2022-08-12 09:41:03 +0100

    signup page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,11 @@ import React from "react";
 export const Navbar = () => {
   return (
     <div
-      style={{ gridTemplateColumns: "1fr 5fr 1fr" }}
+      style={{ gridTemplateColumns: "1fr 3fr 1fr" }}
       class="h-[89px] grid gap-4 pl-[48px] pr-[48px] content-center md:pl-[28px] md:pr-[28px] border-[#E2E2E2] border-b-[2px] mb-[10%] bg-white"
     >
       <div>Carril</div>
-      <div
-        style={{ gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr" }}
-        className="grid pt-[5px] pb-[5px] items-center text-center"
-      >
+      <div className="grid pt-[5px] pb-[5px]  grid-cols-5 items-center text-center">
         <div>
           <div className="bg-[#222F51] pt-[5px]  m-[auto] text-[8px] text-[white] text-center rounded-[50%] h-[25px] w-[25px]">
             1
@@ -19,14 +16,14 @@ export const Navbar = () => {
             Account details
           </p>
         </div>
-        <span className=" border-b-[1px] border-[#E5E5E5]" />
+        <span className=" border-b-[2px] border-[#E5E5E5]" />
         <div>
           <div className="bg-[#E5E5E5] text-[8px] pt-[5px]  m-[auto] text-[white] text-center rounded-[50%]  h-[25px] w-[25px]">
             2
           </div>
           <p className="text-[#E5E5E5] text-[12px] font-semibold">Pricing</p>
         </div>
-        <span className=" border-b-[1px] border-[#E5E5E5]" />
+        <span className=" border-b-[2px] border-[#E5E5E5]" />
         <div>
           <div className="bg-[#E5E5E5] text-[8px] pt-[5px] m-[auto] text-[white] text-center rounded-[50%]  h-[25px] w-[25px]">
             3
