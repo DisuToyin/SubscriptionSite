4271f426c995ef8c31c81f9aceac1eee1313aec5
DisuToyin <disujt@gmail.com>
2022-08-13 16:56:03 +0100

    table work

---
+++ src/components/Table.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Table(props) {
+  return <table class="table-auto">{props.children}</table>;
+}
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -9,6 +9,10 @@ import arrowdown from "../assets/arrowdown.svg";
 import "./styles.css";
 import listIcon from "../assets/listicon.svg";
 import boardview from "../assets/boardview.svg";
+// import TableComp from "../components/Table";
+import tableIcon from "../assets/tableIcon.svg";
+import chatIcon from "../assets/chatBox.svg";
+import ecllipse from "../assets/Ellipse 100.svg";
 
 export default function Project() {
   //   const [showModal, setShowModal] = useState(false);
@@ -46,7 +50,7 @@ export default function Project() {
           </div>
         </div>
       </NavbarComp>
-      <div className="mt-[40px] pl-[120px] pr-[120px]">
+      <div className="mt-[40px] pl-[120px] pr-[120px] md:pl-[28px] md:pr-[28px]">
         <div className="grid grid-cols-2">
           <h1 className="text-[28px] font-bold">Tasks</h1>
           <div className="flex justify-end self-center">
@@ -65,7 +69,6 @@ export default function Project() {
           <p className="hover-underline-animation cursor-pointer hover:text-[#000000] text-[#777777]">
             All
           </p>
-
           <p className="hover-underline-animation cursor-pointer hover:text-[#000000] text-[#777777]">
             Ongoing
           </p>
@@ -78,6 +81,43 @@ export default function Project() {
           <p className="hover-underline-animation  hover:text-[#000000] cursor-pointer text-[#777777]">
             Queued
           </p>
+          {/* <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+            <ul class="flex flex-wrap -mb-px">
+              <li class="mr-2">
+                <a
+                  href="#"
+                  class="inline-block pr-4 pt-4 pb-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                >
+                  Profile
+                </a>
+              </li>
+              <li class="mr-2">
+                <a
+                  href="#"
+                  class="inline-block pr-4 pt-4 pb-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500"
+                  aria-current="page"
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li class="mr-2">
+                <a
+                  href="#"
+                  class="inline-block pr-4 pt-4 pb-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                >
+                  Settings
+                </a>
+              </li>
+              <li class="mr-2">
+                <a
+                  href="#"
+                  class="inline-block pr-4 pt-4 pb-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                >
+                  Contacts
+                </a>
+              </li>
+            </ul>
+          </div>*/}{" "}
         </div>
         <div className="border-[.5px] mt-[.9rem] border-[#E5E5E5] border-b-[#E5E5E5]"></div>
         <div className="grid grid-cols-2 mt-[16px]">
@@ -102,6 +142,139 @@ export default function Project() {
               />
               <p className="col-span-3 text-[#222F51]">Board View</p>
             </div>
+          </div>
+        </div>
+        <div className="mt-8">
+          <div class="overflow-x-auto relative  sm:rounded-[2px]">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase bg-[white] dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="py-3 text-[12px] font-medium px-6">
+                    <div class="flex items-center">
+                      {/* <a href="#"> */}
+                      <img
+                        className="ml-1 w-3 h-3"
+                        src={chatIcon}
+                        alt="table"
+                      />
+                    </div>
+                  </th>
+                  <th scope="col" className="py-3 text-[12px] font-medium px-6">
+                    <div class="flex items-center">
+                      Task name
+                      {/* <a href="#"> */}
+                      <img
+                        className="ml-1 w-3 h-3"
+                        src={tableIcon}
+                        alt="table"
+                      />
+                    </div>
+                  </th>
+                  <th scope="col" className="py-3 text-[12px] font-medium px-6">
+                    <div class="flex items-center">
+                      Assigned To
+                      {/* <a href="#"> */}
+                      <img
+                        className="ml-1 w-3 h-3"
+                        src={tableIcon}
+                        alt="table"
+                      />
+                    </div>
+                  </th>
+                  <th scope="col" className="py-3 text-[12px] font-medium px-6">
+                    <div class="flex items-center">
+                      Status
+                      {/* <a href="#"> */}
+                      <img
+                        className="ml-1 w-3 h-3"
+                        src={tableIcon}
+                        alt="table"
+                      />
+                    </div>
+                  </th>
+                  <th scope="col" className="py-3 text-[12px] font-medium px-6">
+                    <div class="flex items-center">
+                      Delivery date
+                      {/* <a href="#"> */}
+                      <img
+                        className="ml-1 w-3 h-3"
+                        src={tableIcon}
+                        alt="table"
+                      />
+                    </div>
+                  </th>
+                  <th scope="col" className="py-3 text-[12px] font-medium px-6">
+                    <div class="flex items-center">
+                      Category
+                      {/* <a href="#"> */}
+                      <img
+                        className="ml-1 w-3 h-3"
+                        src={tableIcon}
+                        alt="table"
+                      />
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td class="py-4 px-6">
+                    <img src={ecllipse} alt="ellipse" />
+                  </td>
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Astral Projections
+                  </th>
+                  <td class="py-4 px-6">Sliver</td>
+                  <td class="py-4 px-6">Completed</td>
+                  <td class="py-4 px-6">$2999</td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td class="py-4 px-6">
+                    <img src={ecllipse} alt="ellipse" />
+                  </td>
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Astral Projections
+                  </th>
+                  <td class="py-4 px-6">Sliver</td>
+                  <td class="py-4 px-6">Ongoing</td>
+                  <td class="py-4 px-6">$2999</td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td class="py-4 px-6">
+                    <img src={ecllipse} alt="ellipse" />
+                  </td>
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Astral Projections
+                  </th>
+                  <td class="py-4 px-6">White</td>
+                  <td class="py-4 px-6">Queued</td>
+                  <td class="py-4 px-6">$1999</td>
+                </tr>
+                <tr class="bg-white dark:bg-gray-800">
+                  <td class="py-4 px-6">
+                    <img src={ecllipse} alt="ellipse" />
+                  </td>
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Astral Projections
+                  </th>
+                  <td class="py-4 px-6">Black</td>
+                  <td class="py-4 px-6">Accessories</td>
+                  <td class="py-4 px-6">$99</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
