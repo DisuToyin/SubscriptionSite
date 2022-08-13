a2f980dd87d43113404718afe66a35b24006b453
DisuToyin <disujt@gmail.com>
2022-08-13 14:51:28 +0100

    projects page updates

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,10 +3,12 @@ import NavbarComp from "../components/NavbarComp";
 import carrilwhitelogo from "../assets/Carril button.svg";
 import calendlyIcon from "../assets/Icons.svg";
 import bellNoti from "../assets/Button.svg";
-import addIcon from "../assets/addIcon.svg";
+// import addIcon from "../assets/addIcon.svg";
 import arrowdown from "../assets/arrowdown.svg";
+import "./styles.css";
 
 export default function Project() {
+  //   const [showModal, setShowModal] = useState(false);
   return (
     <div>
       <NavbarComp className="bg-[#222F51] items-center h-[60px] grid grid-cols-2 pl-[28px] pt-[10px] pb-[10px] pr-[28px]">
@@ -57,26 +59,28 @@ export default function Project() {
           </div>
         </div>
         <div className="mt-[16px]  text-[14px] text-[#777777] font-bold flex gap-8">
-          <div className="block">
-            <p className="cursor-pointer hover:text-[#000000] text-[#777777]">
-              All
-              <span className="hover:flex hover:border-b-[red] hidden ">
-                red
-              </span>
-            </p>
-          </div>
-          <p className="cursor-pointer hover:text-[#000000] text-[#777777]">
+          <p className="hover-underline-animation cursor-pointer hover:text-[#000000] text-[#777777]">
+            All
+          </p>
+
+          <p className="hover-underline-animation cursor-pointer hover:text-[#000000] text-[#777777]">
             Ongoing
           </p>
-          <p className="cursor-pointer hover:text-[#000000] text-[#777777]">
+          <p className="hover-underline-animation cursor-pointer hover:text-[#000000] text-[#777777]">
             Completed
           </p>
-          <p className=" cursor-pointer hover:text-[#000000] text-[#777777]">
+          <p className=" hover-underline-animation cursor-pointer hover:text-[#000000] text-[#777777]">
             Drafts
           </p>
-          <p className="hover:text-[#000000] text-[#777777]">Queued</p>
+          <p className="hover-underline-animation  hover:text-[#000000] cursor-pointer text-[#777777]">
+            Queued
+          </p>
         </div>
-        <span className=" border-b-[2px] border-[#E5E5E5]" />
+        <div className="border-[1px] mt-[.9rem] border-[#E5E5E5] border-b-[#E5E5E5]"></div>
+        <div className="grid grid-cols-2 mt-[16px]">
+          <div className="flex"></div>
+          <div className="flex"></div>
+        </div>
       </div>
     </div>
   );
