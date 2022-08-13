504210db9a2a39d4d55df12e7987b2bb9b335be1
DisuToyin <disujt@gmail.com>
2022-08-13 13:57:52 +0100

    yup

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,6 +3,7 @@ import NavbarComp from "../components/NavbarComp";
 import carrilwhitelogo from "../assets/Carril button.svg";
 import calendlyIcon from "../assets/Icons.svg";
 import bellNoti from "../assets/Button.svg";
+import addIcon from "../assets/addIcon.svg";
 import arrowdown from "../assets/arrowdown.svg";
 
 export default function Project() {
@@ -40,6 +41,43 @@ export default function Project() {
           </div>
         </div>
       </NavbarComp>
+      <div className="mt-[40px] pl-[120px] pr-[120px]">
+        <div className="grid grid-cols-2">
+          <h1 className="text-[28px] font-bold">Tasks</h1>
+          <div className="flex justify-end self-center">
+            <input
+              className="rounded-[2px] outline-none border-[1px] mr-[5%] h-[32px] w-[50%] pl-[18px] text-[12px] border-[#E2E2E2]"
+              placeholder="Search by task name"
+            />
+            <button className="flex pt-[5px] text-[white] h-[32px] rounded-[2px] text-[12px] pl-[10px] pr-[10px] font-medium bg-[#E9724C]">
+              {" "}
+              {/* <img alt="add icon" src={addIcon} />  */}
+              <span> New task</span>
+            </button>
+          </div>
+        </div>
+        <div className="mt-[16px]  text-[14px] text-[#777777] font-bold flex gap-8">
+          <div className="block">
+            <p className="cursor-pointer hover:text-[#000000] text-[#777777]">
+              All
+              <span className="hover:flex hover:border-b-[red] hidden ">
+                red
+              </span>
+            </p>
+          </div>
+          <p className="cursor-pointer hover:text-[#000000] text-[#777777]">
+            Ongoing
+          </p>
+          <p className="cursor-pointer hover:text-[#000000] text-[#777777]">
+            Completed
+          </p>
+          <p className=" cursor-pointer hover:text-[#000000] text-[#777777]">
+            Drafts
+          </p>
+          <p className="hover:text-[#000000] text-[#777777]">Queued</p>
+        </div>
+        <span className=" border-b-[2px] border-[#E5E5E5]" />
+      </div>
     </div>
   );
 }
